84c6bd34277d4898814c3ea5ffe5a8284ac3c31d
Aaron Langham <124449717+Aaron-Langham@users.noreply.github.com>
2023-09-22 13:12:56 +0100

    Update and rename Monitor Old AD Users 1.1.1.ps1 to Monitor-OldADUsersV1.1.1.ps1

--- Disable-OldUsers\Computers/Monitor-OldADUsersV1.1.1.ps1
+++ Disable-OldUsers\Computers/Monitor-OldADUsersV1.1.1.ps1
@@ -4,12 +4,7 @@ $DomainCheck = Get-CimInstance -ClassName Win32_OperatingSystem
 if ($DomainCheck.ProductType -ne "2") { Write-Host "Not a domain controller. Soft exiting." ; exit 0 }
 
 $blacklistU = @(
-"Administrator"
-"Disc"
-"SQL"
-"DFWTechSupport"
-"SOE"
-"soe")
+"Administrator")
 
 $OldUsers = Get-ADuser -Filter * -properties UserPrincipalName, Enabled, WhenCreated, LastLogonDate | Select-Object UserPrincipalName, Enabled, WhenCreated, LastLogonDate | Where-Object { $_.LastLogonDate -lt $age } | Where-Object { $_.Enabled -eq $True}| Where-Object { $_.UserPrincipalName -ne $null} | Where-Object { ($_.UserPrincipalName -split "@")[0] -notin $blacklistU} | Where-Object { $_.WhenCreated -lt ((Get-Date).AddDays(-14))}
 
