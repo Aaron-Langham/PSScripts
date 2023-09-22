353e1c58900815a10e8e366238923edf28c38d91
Aaron Langham <124449717+Aaron-Langham@users.noreply.github.com>
2023-09-22 12:44:15 +0100

    Create  Increase-MaxOSTFileLimit100GB.bat

---
+++ Increase-MaxOSTFileLimit100GB.bat
@@ -0,0 +1,2 @@
+REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Outlook\PST" /v MaxLargeFileSize /t REG_DWORD /d "102400" /f
+REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Outlook\PST" /v WarnLargeFileSize /t REG_DWORD /d "97280" /f
