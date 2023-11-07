37bf519538c4182a06b0b9dc8173e0cfacdd9896
Aaron Langham <124449717+Aaron-Langham@users.noreply.github.com>
2023-11-07 09:07:05 +0000

    Delete Monitor-ActiveDirectoryRecycleBinV2.0.ps1

--- Monitor-ActiveDirectoryRecycleBinV2.0.ps1
+++
@@ -1,4 +0,0 @@
-$Recycler = Get-ADOptionalFeature -Filter 'name -like "Recycle Bin Feature"'
-
-if(!$Recycler.EnabledScopes){Write-Host "Active Directory Recycle Bin is not enabled"; exit 1}
-else{Write-Host "Healthy"}
