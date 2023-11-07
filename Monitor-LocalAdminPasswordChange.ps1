91d277683a905c9cc5524270509b417c9cdc4899
Aaron Langham <124449717+Aaron-Langham@users.noreply.github.com>
2023-11-07 10:08:45 +0000

    Create Monitor-LocalAdminPasswordChange.ps1

---
+++ Monitor-LocalAdminPasswordChange.ps1
@@ -0,0 +1,15 @@
+$version = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").CurrentVersion
+if($Version -lt "6.3") {
+  write-DRRMAlert "Unsupported OS. Only Server 2012R2 or 8.1 and up are supported."
+  exit 0
+}
+
+$LastDay = (Get-Date).addhours(-24)
+$AdminGroup = Get-LocalGroupMember -SID "S-1-5-32-544"
+
+$ChangedAdmins = foreach($Admin in $AdminGroup){
+  get-localuser -ErrorAction SilentlyContinue -sid $admin.sid | Where-Object {$_.PasswordLastSet -gt $LastDay} 
+}
+
+if  (!$ChangedAdmins){write-host "Healthy"}
+else {write-host "Unhealthy. Please check diagnostics"; write-host ($ChangedAdmins | fl *); exit 1}
