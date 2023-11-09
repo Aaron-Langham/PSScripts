f7c6bbf3bf96694e4654badd74a1ab528e4111f2
Aaron Langham <124449717+Aaron-Langham@users.noreply.github.com>
2023-11-09 10:36:18 +0000

    Update Remove-McAfee.ps1

--- Remove-McAfee.ps1
+++ Remove-McAfee.ps1
@@ -1,2 +1,4 @@
+# Removes McAfee Apps
+
 $McAfeeApps = Get-AppxPackage -Name "*McAfee*" -AllUsers | Select-Object *
 foreach ($app in $McAfeeApps){Remove-AppxPackage -Package $app.PackageFullName -AllUsers}
