9f511cc55f940db4395d824388a63145a87826a1
Aaron Langham <124449717+Aaron-Langham@users.noreply.github.com>
2023-11-09 10:08:34 +0000

    Update Get-Resolution.ps1

--- Get-Resolution.ps1
+++ Get-Resolution.ps1
@@ -1,2 +1,4 @@
-﻿$thisPC = hostname; Get-WmiObject -Class Win32_DesktopMonitor | Select-Object ScreenWidth,ScreenHeight
+# Gets the Screen resolution of a PC
+ 
+$thisPC = hostname; Get-WmiObject -Class Win32_DesktopMonitor | Select-Object ScreenWidth,ScreenHeight
 $thisPC
