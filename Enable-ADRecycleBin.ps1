585777c20e246e65ff0321438e0ce3f07cf63a2a
Aaron Langham <124449717+Aaron-Langham@users.noreply.github.com>
2023-11-09 10:05:27 +0000

    Update Enable-ADRecycleBin.ps1

--- Enable-ADRecycleBin.ps1
+++ Enable-ADRecycleBin.ps1
@@ -1,5 +1,4 @@
 # Enables the Active Directory Recycle Bin Feature.
 
 Enable-ADOptionalFeature -Confirm -Identity 'Recycle Bin Feature' -Scope 'ForestOrConfigurationSet' -Target (Get-ADDomain).Forest -Server ((HOSTNAME) + "." + (Get-ADDomain).Forest)
-
 Enable-ADOptionalFeature "Recycle Bin Feature" -server ((Get-ADForest -Current LocalComputer).DomainNamingMaster) -Scope ForestOrConfigurationSet -Target (Get-ADForest -Current LocalComputer) 
