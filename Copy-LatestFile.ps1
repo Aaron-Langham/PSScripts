3dddf173388481f5fa7076e06e9c64b1853597dc
Aaron Langham <124449717+Aaron-Langham@users.noreply.github.com>
2023-11-09 10:01:20 +0000

    Update Copy-LatestFile.ps1

--- Copy-LatestFile.ps1
+++ Copy-LatestFile.ps1
@@ -1,5 +1,7 @@
-$targetPath = ""
-$outputPath = ""
+# Looks at the latest file in a given directory and then copies it to a second given directory. 
+
+$targetPath = "" # Source directory
+$outputPath = "" # Output directory
 
 Set-Location $targetPath
 $latestFile = (Get-ChildItem -Path $targetPath -Attributes !Directory | Sort-Object -Descending -Property LastWriteTime | Select-Object -First 1)
