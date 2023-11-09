add336db862ffbb6007327d386f7841fca55e8ff
Aaron Langham <124449717+Aaron-Langham@users.noreply.github.com>
2023-11-09 10:07:05 +0000

    Update Get-InstalledPrograms.ps1

--- Get-InstalledPrograms.ps1
+++ Get-InstalledPrograms.ps1
@@ -1,1 +1,3 @@
-﻿Get-WmiObject -Class Win32_Product -Property Name | Select-Object name
+# Gets a list of installed programs
+ 
+ Get-WmiObject -Class Win32_Product -Property Name | Select-Object name
